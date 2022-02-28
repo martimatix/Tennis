48bf6dca3cefc59d27f60854230337643134ebd3
Mario Martinez <zenitram.oiram@gmail.com>
2022-03-01 06:14:10 +0800

    Create win_n_points helper

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -2,6 +2,13 @@ extern crate tennis;
 
 const POINTS_TO_WIN_GAME: u8 = 4;
 
+pub fn win_n_points(mut tennis_match: tennis::Match, player_name: &str, n: usize) -> tennis::Match {
+    for _ in 0..n {
+        tennis_match.point_won_by(player_name.to_string());
+    }
+    tennis_match
+}
+
 pub fn win_n_games(mut tennis_match: tennis::Match, player_name: &str, n: u8) -> tennis::Match {
     for _ in 0..(n * POINTS_TO_WIN_GAME) {
         tennis_match.point_won_by(player_name.to_string());
--- tests/tennis.rs
+++ tests/tennis.rs
@@ -29,24 +29,16 @@ fn test_score_40_15() {
 #[test]
 fn test_score_deuce() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 3);
+    tennis_match = helpers::win_n_points(tennis_match, "player 2", 3);
     assert_eq!(tennis_match.score(), "0-0, Deuce");
 }
 
 #[test]
 fn test_score_advantage_player_1() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 3);
+    tennis_match = helpers::win_n_points(tennis_match, "player 2", 3);
     tennis_match.point_won_by("player 1".to_string());
     assert_eq!(tennis_match.score(), "0-0, Advantage player 1");
 }
@@ -54,38 +46,23 @@ fn test_score_advantage_player_1() {
 #[test]
 fn test_player_1_wins_first_tennis_set() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 4);
     assert_eq!(tennis_match.score(), "1-0");
 }
 
 #[test]
 fn test_player_1_wins_first_tennis_set_from_deuce() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 3);
+    tennis_match = helpers::win_n_points(tennis_match, "player 2", 3);
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 2);
     assert_eq!(tennis_match.score(), "1-0");
 }
 
 #[test]
 fn test_player_1_wins_first_two_tennis_sets() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 8);
     assert_eq!(tennis_match.score(), "2-0");
 }
 
@@ -108,12 +85,8 @@ fn test_score_tie_break_1_0() {
 fn test_score_tie_break_3_3() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
     tennis_match = helpers::initialize_tie_break(tennis_match, "player 1", "player 2");
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 3);
+    tennis_match = helpers::win_n_points(tennis_match, "player 2", 3);
     assert_eq!(tennis_match.score(), "6-6, 3-3");
 }
 
@@ -121,13 +94,7 @@ fn test_score_tie_break_3_3() {
 fn test_win_tie_break_game() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
     tennis_match = helpers::initialize_tie_break(tennis_match, "player 1", "player 2");
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 7);
     assert_eq!(tennis_match.score(), "7-6, player 1 wins");
 }
 
@@ -135,18 +102,8 @@ fn test_win_tie_break_game() {
 fn test_score_tie_break_7_6() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
     tennis_match = helpers::initialize_tie_break(tennis_match, "player 1", "player 2");
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 6);
+    tennis_match = helpers::win_n_points(tennis_match, "player 2", 6);
     tennis_match.point_won_by("player 1".to_string());
     assert_eq!(tennis_match.score(), "6-6, 7-6");
 }
@@ -155,20 +112,9 @@ fn test_score_tie_break_7_6() {
 fn test_win_tie_break_game_with_more_than_seven_points() {
     let mut tennis_match = tennis::Match::new("player 1".to_string(), "player 2".to_string());
     tennis_match = helpers::initialize_tie_break(tennis_match, "player 1", "player 2");
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 2".to_string());
-    tennis_match.point_won_by("player 1".to_string());
-    tennis_match.point_won_by("player 1".to_string());
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 6);
+    tennis_match = helpers::win_n_points(tennis_match, "player 2", 6);
+    tennis_match = helpers::win_n_points(tennis_match, "player 1", 2);
     assert_eq!(tennis_match.score(), "7-6, player 1 wins");
 }
 
